d8c8a4fe608d02192bd36543355e1010dcc5b761
Whermerson19 <whermersonc@gmail.com>
2021-04-15 15:43:28 -0300

    add typescript, sass and extern fonts

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+
+import styles from "../styles/home.module.scss";
+
+export default function Home() {
+  return (
+    <h1 className={styles.title}>
+      Hello <span>world</span>
+    </h1>
+  );
+}
