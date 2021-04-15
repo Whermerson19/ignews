f0df7e1603e15624747328bee112f278a7b20017
Whermerson19 <whermersonc@gmail.com>
2021-04-15 19:19:35 -0300

    home page design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,28 @@
 import Head from "next/head";
 
+import style from "./home.module.scss";
+
 export default function Home() {
   return (
-    <h1>Welcome Whermerson</h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={style.contentContainer}>
+        <section className={style.hero}>
+          <span>Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications for <br />
+            <span> R$ 9,90/month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
   );
 }
